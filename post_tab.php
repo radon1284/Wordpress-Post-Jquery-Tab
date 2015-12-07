7491bc42d59fb4e846b82f746f6e2fde939bd5a6
radongrafix <radon1284@yahoo.com>
2015-12-07 21:36:29 +0800

    Update post_tab.php

--- post_tab.php
+++ post_tab.php
@@ -54,3 +54,40 @@ $query = new WP_Query( array(
     }
 }
 add_shortcode( 'dynamic_section', 'dynamic_shortcode' );
+
+// Jquery UI tab
+
+// Register Script
+function custom_scripts() {
+
+	wp_register_script( 'jquery-ui', '//code.jquery.com/ui/1.11.4/jquery-ui.js', false, false, true );
+	wp_enqueue_script( 'jquery-ui' );
+
+	wp_register_script( 'jquery-1.10.2', '//code.jquery.com/jquery-1.10.2.js', false, false, true );
+	wp_enqueue_script( 'jquery-1.10.2' );
+
+}
+add_action( 'wp_enqueue_scripts', 'custom_scripts' );
+  
+// Register Style
+function custom_styles() {
+
+	wp_register_style( 'style_name', get_template_directory_uri() . '/resources/demos/style.css', false, false );
+	wp_enqueue_style( 'style_name' );
+
+	wp_register_style( 'jquery-ui', '//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css', false, false );
+	wp_enqueue_style( 'jquery-ui' );
+
+}
+add_action( 'wp_enqueue_scripts', 'custom_styles' );
+
+  
+  // Adding Script into Foote php
+  function add_script(){ ?>
+  <script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+  </script>
+  <?php }
+add_action('wp_footer', 'add_script');
